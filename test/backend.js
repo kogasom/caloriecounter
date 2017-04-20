cf2146f93edb49041dde57fa24c04d50034c138a
Koltai G. Soma <koltai.soma@gmail.com>
2017-04-20 21:22:50 +0200

    Login tests

--- test/backend.js
+++ test/backend.js
@@ -12,6 +12,12 @@ describe('Server started', function () {
   })
 })
 
+var user = {
+    name: 'name_'+randomString(10),
+    username: 'username_'+randomString(10),
+    password: 'pass_'+randomString(10)
+}
+
 describe('Registration', function () {
     it('should return response.errors when required fields are missing', function (done) {
         request
@@ -53,19 +59,11 @@ describe('Registration', function () {
     })
 
     it('should register a new user', function (done) {
-        var user = {
-            name: 'name_'+randomString(10),
-            username: 'username_'+randomString(10),
-            password: 'pass_'+randomString(10)
-        }
         request
         .post('/auth/register')
         .send(user)
         .expect(function(res){
-            if (!res.body.user) {
-                console.log(res.body)
-                throw new Error('no response.user')
-            }
+            if (!res.body.user) throw new Error('no response.user')
             if (res.body.user.name != user.name) throw new Error('wrong user.name')
             if (res.body.user.username != user.username) throw new Error('wrong user.username')
             if (res.body.user.password != user.password) throw new Error('wrong user.password')
@@ -100,6 +98,33 @@ describe('Login', function () {
         .expect(function(res){
             if (!res.body.errors) throw new Error('no response.errors')
             if (!res.body.errors.password) throw new Error('no errors.password in response')
+        })
+        .end(done)
+    })
+
+    it('should not log in with wrong credentials', function (done) {
+        request
+        .post('/auth/login')
+        .send({
+            username: user.username,
+            password: randomString(10)
+        })
+        .expect(function(res){
+            if (!res.body.errors) throw new Error('no response.errors')
+            if (!res.body.errors.auth) throw new Erorr('no errors.auth in response')
+        })
+        .end(done)
+    })
+
+    it('should log in and receive response.user', function (done) {
+        request
+        .post('/auth/login')
+        .send({
+            username: user.username,
+            password: user.password
+        })
+        .expect(function(res){
+            if (!res.body.user) throw new Error('no response.users')
         })
         .end(done)
     })
